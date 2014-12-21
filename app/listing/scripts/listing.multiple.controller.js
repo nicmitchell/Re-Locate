e6810c85d5679b275e793ede6f48dae33f765f93
Nic Mitchell <nic@nicmitchell.com>
2014-12-20 22:55:53 -0700

    hardcoded listing fixtures in listing.services.js

--- app/listing/scripts/listing.multiple.controller.js
+++ app/listing/scripts/listing.multiple.controller.js
@@ -1,5 +1,17 @@
 angular
   .module('listing.multiple')
-  .controller('ListingMultipleController', function($scope, supersonic) {
-    // Controller functionality here
+  .controller('ListingMultipleController', function($scope, supersonic, Listings) {
+    // model
+    $scope.listings = {};
+
+    // load listings via $http
+    // $scope.getListings = function(){
+    //   Listings.getListings().then(function(data){
+    //     $scope.listings = data;
+    //   });
+    // };
+    $scope.getListings = function(){
+      $scope.listings = Listings.getListings();
+    };
+    $scope.getListings();
   });
