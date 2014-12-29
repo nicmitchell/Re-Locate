7574dd04e104b7af1e2d8fb0e809a2fc2aa071f8
Nic Mitchell <nic@nicmitchell.com>
2014-12-29 12:06:15 -0700

    removed sample code

--- app/listing/scripts/listing-multiple-controller.js
+++ app/listing/scripts/listing-multiple-controller.js
@@ -3,13 +3,6 @@ angular
   .controller('ListingMultipleController', function($scope, supersonic, Listings) {
     // model
     $scope.listings = {};
-
-    // load listings via $http
-    // $scope.getListings = function(){
-    //   Listings.getListings().then(function(data){
-    //     $scope.listings = data;
-    //   });
-    // };
     $scope.getListings = function(){
       $scope.listings = Listings.getListings();
       console.log('get listings called');
