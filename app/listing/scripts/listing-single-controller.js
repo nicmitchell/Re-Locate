0122d3af1e5d7166674b04d64f78e514c7ed25a6
Nic Mitchell <nic@nicmitchell.com>
2014-12-29 19:03:21 -0700

    created fixture model for sample data

--- app/listing/scripts/listing-single-controller.js
+++ app/listing/scripts/listing-single-controller.js
@@ -1,5 +1,32 @@
 angular
   .module('listing')
-  .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+  .controller('ListingSingleController', function($scope, supersonic, Listings) {
+
+    $scope.listing = null;
+    $scope.test = 'test';
+    $scope.showSpinner = true;
+    $scope.dataId = void 0;
+    _refreshViewData = function() {
+      steroids.logger.log('_refreshViewData called', $scope.test);
+      return Listings.find($scope.dataId).then(function(listing) {
+        return $scope.$apply(function() {
+          $scope.listing = listing;
+          $scope.showSpinner = false;
+          steroids.logger.log('listing', listing);
+        });
+      });
+    };
+    supersonic.ui.views.current.whenVisible(function() {
+      if ($scope.dataId) {
+        return _refreshViewData();
+      }
+    });
+    supersonic.ui.views.current.params.onValue(function(values) {
+      steroids.logger.log('on values', values);
+      $scope.dataId = values.id;
+      steroids.logger.log('$scope.dataID', $scope.dataId);
+      _refreshViewData();
+    }); 
+    // _refreshViewData();
+    // steroids.logger.log('listing at controller load', $scope.listing);
   });
