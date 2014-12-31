7fd82876273a23d097f4fa51feb7ebdd44959231
Nic Mitchell <nic@nicmitchell.com>
2014-12-31 12:58:20 -0700

    adding sample data into localForage

--- app/listing/index.js
+++ app/listing/index.js
@@ -1,6 +1,20 @@
 angular.module('listing', ['common', 'listing.services'])
-.config(['$localForageProvider', function($localForageProvider){
+.config(['$localForageProvider', function($localForageProvider, $http){
   $localForageProvider.config({
-    storeName   : 'listings' // name of the table
+    storeName: 'listings' // name of the table
   });
-}]);
+}])
+.run(function($http, $localForage){
+  return $http({
+    method: 'GET',
+    url: '/listing-samples.js'
+  })
+  .then(function(res){
+    console.log('response data', res.data);
+    console.log('response data length', res.data.length);
+    for(var i = 0, l = res.data.length; i < l; i ++){
+      $localForage.setItem(res.data[i].ListingID, res.data[i]);
+    }
+    return res.data;
+  });
+})
--- app/listing/scripts/listing-services.js
+++ app/listing/scripts/listing-services.js
@@ -1,25 +1,27 @@
 angular.module('listing.services', ['common'])
 
 .factory('Listings', function ($http, $localForage) {
+  // this should be changed for search functionality
+  // take an input of search variables, retrieve from local storage, attach to $scope.listings
   var getListings = function(){ 
     // $http().then(response data)
-    return $http({
-      method: 'GET',
-      url: '/listing-samples.js'
-    })
-    .then(function(res){
-      console.log('response data', res.data);
-      console.log('response data length', res.data.length);
-      for(var i = 0, l = res.data.length; i < l; i ++){
-        $localForage.setItem(res.data[i].ListingID, res.data[i]);
-      }
-      // $localForage.setItem('myName','Olivier Combe').then(function() {
-      //   $localForage.getItem('myName').then(function(data) {
-      //     var myName = data;
-      //   });
-      // });
-      return res.data;
-    });
+    // return $http({
+    //   method: 'GET',
+    //   url: '/listing-samples.js'
+    // })
+    // .then(function(res){
+    //   console.log('response data', res.data);
+    //   console.log('response data length', res.data.length);
+    //   for(var i = 0, l = res.data.length; i < l; i ++){
+    //     $localForage.setItem(res.data[i].ListingID, res.data[i]);
+    //   }
+    //   // $localForage.setItem('myName','Olivier Combe').then(function() {
+    //   //   $localForage.getItem('myName').then(function(data) {
+    //   //     var myName = data;
+    //   //   });
+    //   // });
+    //   return res.data;
+    // });
   };
   return { 
     getListings: getListings
