3e6394f3d22eb0e91d8d6f8b64b74012c0190df6
Brett DeWoody <brett@digitalwaxworks.com>
2015-01-06 20:50:58 -0700

    Restict API fields to the minimum.

--- web/app/controllers/api/v1/listings_controller.rb
+++ web/app/controllers/api/v1/listings_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ListingsController < ApplicationController
       def index
-        @listings = Listing.paginate(:page => params[:page], :per_page => 100)
+        @listings = Listing.select('price, beds, baths, year, sqft_max, address_fields, extra, premium, mls_num').paginate(:page => params[:page], :per_page => 200)
         render :json => @listings
       end
     end
