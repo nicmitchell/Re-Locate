8ab6322583413300b4000f6b3fffefa99bad8e74
Nic Mitchell <nic@nicmitchell.com>
2015-01-09 22:07:05 -0800

    (feature) added pagination and filter to homes index

--- app/home/scripts/IndexController.js
+++ app/home/scripts/IndexController.js
@@ -3,6 +3,7 @@ angular
   .controller("IndexController", function ($scope, Home, supersonic) {
     $scope.homes = null;
     $scope.showSpinner = true;
+    $scope.currentPage = 1;
 
     Home.all().whenChanged( function (homes) {
         $scope.$apply( function () {
