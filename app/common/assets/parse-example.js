34926876fad70dbb5d32cc5ff028396ed6a36e1c
Nic Mitchell <nic@nicmitchell.com>
2015-04-25 19:47:59 -0700

    added instructions

--- app/common/assets/parse-example.js
+++ app/common/assets/parse-example.js
@@ -1,2 +1,4 @@
-// Go to Parse.com > Your Application > Keys to get the values for the following
+// Go to Parse.com > Your Application > Keys to get the values for the "Application ID" and "JavaScript Key"
+// Paste the values below and uncomment the line
+// Rename this file to 'parse.js'
 // Parse.initialize('Application ID', 'JavaScript Key');
